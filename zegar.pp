43e1ee5835c1c41561d02851324cb2057ac39e52
Artur Hebda <artur.hebda@adtaily.com>
2011-01-06 15:01:47 +0100

    Dodanie obsługi przypisywania i wyświetlania czasu

--- zegar.pp
+++ zegar.pp
@@ -45,7 +45,28 @@ begin
   current_date.wday := wday;
 end;
 
-{ METHOD: parse_date_to_readable_form - metoda zwracająca datę w postaci np "1 stycznia 1970r." }
+{ METHOD: set_current_time - metoda ustawia aktualny czas jako wartość zmiennej current_time }
+procedure set_current_time;
+begin
+  gettime(hours, minutes, seconds, cseconds);
+  current_time.hours := hours;
+  current_time.minutes := minutes;
+  current_time.seconds := seconds;
+  current_time.cseconds := cseconds;
+end;
+
+{ METHOD: add_zero_if_smaller_than_ten - metoda zwracająca string podany jako argument z dodanym zerem, jeśli wartość stringa była mniejsza niż 10 }
+function add_zero_if_smaller_than_ten(str : string):string;
+var
+  wartosc : word;
+
+begin
+  val(str, wartosc);
+  if (wartosc < 10) then str := '0' + str;
+  add_zero_if_smaller_than_ten := str;
+end;
+
+{ METHOD: parse_date_to_readable_form - metoda zwracająca datę w postaci np. "1 stycznia 1970r." }
 function parse_date_to_readable_form(date : date):string;
 var
   readable : string;
@@ -57,6 +78,26 @@ begin
   readable := str_day + ' ' + names_of_months[date.month] + ' ' + str_year + 'r.';
 
   parse_date_to_readable_form := readable;
+end;
+
+{ METHOD: parse_time_to_readable_form - metoda zwracająca czas w postaci np. "00:01:10" lub "00:01:10.99" w zależności od parametru precision:
+  false - ograniczenie do sekund; true - dokładny czas (wraz z centysekundami) }
+function parse_time_to_readable_form(time : time; precision : boolean):string;
+var
+  readable : string;
+  str_hours, str_minutes, str_seconds, str_cseconds : string;
+
+begin
+  str(time.hours, str_hours);
+  str(time.minutes, str_minutes);
+  str(time.seconds, str_seconds);
+  str(time.cseconds, str_cseconds);
+  readable := add_zero_if_smaller_than_ten(str_hours) + ':';
+  readable += add_zero_if_smaller_than_ten(str_minutes) + ':';
+  readable += add_zero_if_smaller_than_ten(str_seconds);
+  if (precision) then readable += '.' + add_zero_if_smaller_than_ten(str_cseconds);
+
+  parse_time_to_readable_form := readable;
 end;
 
 { BLOCK1: VIEW - blok odpowiedzialny za wyświetlanie wyników działania programu }
@@ -89,8 +130,19 @@ begin
   textcolor(normal_color);
 end;
 
+{ METHOD: print_current_time - metoda wyświetlająca aktualny czas,
+  UWAGA! czas musi zostać wcześniej ustawiony! }
+procedure print_current_time;
+begin
+  textcolor(info_color);
+  writeln('Godzina: ', parse_time_to_readable_form(current_time, false));
+  textcolor(normal_color);
+end;
+
 begin
   greeting;
   set_current_date;
   print_current_date_and_wday;
+  set_current_time;
+  print_current_time;
 end.
